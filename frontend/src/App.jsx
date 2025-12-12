bfc744f67bc61be42e50571347e38798503c86a4
LucianoLupo95 <lnlupo95@gmail.com>
2025-12-12 23:04:30 +0100

    Seteado polling HTML

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,24 @@ function App() {
   const [metrics, setMetrics] = useState(null);
 
   useEffect(() => {
-    fetch("http://localhost:8080/metrics")
-      .then((res) => {
-        if (res.status === 204) return null;
-        return res.json();
-      })
-      .then((data) => {
-        setMetrics(data);
-      })
-      .catch((err) => {
-        console.error("Error fetching metrics:", err);
-      });
+    const fetchMetrics = () => {
+      fetch("http://localhost:8080/metrics")
+        .then((res) => {
+          if (res.status === 204) return null;
+          return res.json();
+        })
+        .then((data) => {
+          setMetrics(data);
+        })
+        .catch((err) => {
+          console.error("Error fetching metrics:", err);
+        });
+    };
+
+    fetchMetrics(); // primera llamada inmediata
+    const interval = setInterval(fetchMetrics, 3000);
+
+    return () => clearInterval(interval);
   }, []);
 
   return (
